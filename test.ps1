459cee986f0b9cb954d3351248d4eb25d2cc0ba9
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-09-18 22:07:45 +0300

    Try to fix AppVeyor.

--- test.ps1
+++ test.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 dotnet pack Covarsky/Covarsky.csproj -o Covarsky.Tests/test-packages /p:Version=0.0.0-test
 dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-1.binlog
 dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-2.binlog
