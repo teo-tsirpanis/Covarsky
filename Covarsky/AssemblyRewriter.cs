68d8bf579fa37ce9af19ebe1bfa0aa7a298fa7fe
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-09-18 16:02:31 +0300

    Use Sigourney.
    Covarsky's codebase was significantly shrunk.

--- Covarsky/AssemblyRewriter.cs
+++ Covarsky/AssemblyRewriter.cs
@@ -5,53 +5,20 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using Mono.Cecil;
+using Mono.Cecil.Rocks;
+using ILogger = Serilog.ILogger;
 
 namespace Covarsky
 {
-    public class AssemblyRewriter
+    public static class AssemblyRewriter
     {
         private const string ContravariantIn = "ContravariantInAttribute";
         private const string CovariantOut = "CovariantOutAttribute";
-        private const string ProcessedByCovarsky = "ProcessedByCovarsky";
 
-        private readonly AssemblyDefinition _assembly;
-        private readonly TaskLoggingHelper? _log;
-        private readonly List<TypeDefinition> _types;
-        private readonly TypeDefinition? _attributeCovariant;
-        private readonly TypeDefinition? _attributeContravariant;
-
-        private AssemblyRewriter(AssemblyDefinition assembly, string? customInName, string? customOutName,
-            TaskLoggingHelper? log)
-        {
-            _assembly = assembly;
-            _log = log;
-            _types = DiscoverTypes();
-            _attributeCovariant = FindSuitableAttribute(customInName ?? CovariantOut);
-            _attributeContravariant = FindSuitableAttribute(customOutName ?? ContravariantIn);
-        }
-
-        private List<TypeDefinition> DiscoverTypes()
-        {
-            var types = new List<TypeDefinition>();
-
-            void DiscoverNestedTypes(TypeDefinition type)
-            {
-                types.Add(type);
-                foreach (var t in type.NestedTypes)
-                    DiscoverNestedTypes(t);
-            }
-
-            foreach (var t in _assembly.Modules.SelectMany(m => m.Types)) DiscoverNestedTypes(t);
-            return types;
-        }
-
-        private TypeDefinition? FindSuitableAttribute(string name) =>
-            _types.Find(type =>
+        private static TypeDefinition? FindSuitableAttribute(List<TypeDefinition> types, string name) =>
+            types.Find(type =>
                 // Looks like interfaces do not inherit from Object.
                 type.BaseType?.FullName == typeof(Attribute).FullName
                 && type.FullName == name
@@ -62,18 +29,19 @@ namespace Covarsky
             return type.IsInterface || type.BaseType.FullName == typeof(MulticastDelegate).FullName;
         }
 
-        private bool ApplyVariance(TypeDefinition type)
+        private static bool ApplyVariance(TypeDefinition type, TypeDefinition? attributeCovariant,
+            TypeDefinition? attributeContravariant, ILogger? log)
         {
-            static bool hasAttribute(GenericParameter g, TypeDefinition? attributeType) =>
-                g.CustomAttributes.Any(attr => attr.AttributeType == attributeType);
+            static bool HasAttribute(GenericParameter g, TypeDefinition? attributeType) =>
+                attributeType != null
+                && g.CustomAttributes.Any(attr => attr.AttributeType == attributeType);
 
-            void patchGeneric(GenericParameter g, bool doIt, GenericParameterAttributes attr)
+            void PatchGeneric(GenericParameter g, bool doIt, GenericParameterAttributes attr)
             {
                 if (doIt)
                 {
                     g.Attributes = (g.Attributes & ~ GenericParameterAttributes.VarianceMask) | attr;
-                    _log?.LogMessage(MessageImportance.High, "Marking {0}'s {1} as {2}...",
-                        type.FullName, g.Name, attr);
+                    log?.Information("Marking {0}'s {1} as {2}...", type.FullName, g.Name, attr);
                 }
             }
 
@@ -82,25 +50,25 @@ namespace Covarsky
             if (!IsSuitableForVariance(type)) return false;
             foreach (var g in type.GenericParameters)
             {
-                var isCovariant = hasAttribute(g, _attributeCovariant);
-                var isContravariant = hasAttribute(g, _attributeContravariant);
+                var isCovariant = HasAttribute(g, attributeCovariant);
+                var isContravariant = HasAttribute(g, attributeContravariant);
 
                 if (!g.IsNonVariant)
                 {
-                    _log?.LogWarning("Type {0}'s parameter {1} is already variant and it will be ignored.",
+                    log?.Warning("Type {0}'s parameter {1} is already variant and it will be ignored.",
                         type.FullName, g.Name);
                     return false;
                 }
 
                 if (isCovariant && isContravariant)
                 {
-                    _log?.LogError("Type {0}'s parameter {1} cannot be declared as both covariant and contravariant.",
+                    log?.Error("Type {0}'s parameter {1} cannot be declared as both covariant and contravariant.",
                         type.FullName, g.Name);
                     return false;
                 }
 
-                patchGeneric(g, isCovariant, GenericParameterAttributes.Covariant);
-                patchGeneric(g, isContravariant, GenericParameterAttributes.Contravariant);
+                PatchGeneric(g, isCovariant, GenericParameterAttributes.Covariant);
+                PatchGeneric(g, isContravariant, GenericParameterAttributes.Contravariant);
 
                 hasVarianceChanged |= isCovariant || isContravariant;
             }
@@ -108,59 +76,15 @@ namespace Covarsky
             return hasVarianceChanged;
         }
 
-        private bool ShouldRewriteAssembly()
+        public static bool DoWeave(AssemblyDefinition asm, ILogger? log, string? customOutName = null, string? customInName = null)
         {
-            return !_types.Exists(td => td.FullName == ProcessedByCovarsky);
-        }
-
-        private void MarkAsProcessedByCovarsky()
-        {
-            _log?.LogMessage($"Adding the {ProcessedByCovarsky} class...");
-            const TypeAttributes typeAttributes =
-                TypeAttributes.NotPublic | TypeAttributes.Abstract | TypeAttributes.Sealed;
-            var td = new TypeDefinition("", ProcessedByCovarsky, typeAttributes,
-                _assembly.MainModule.TypeSystem.Object);
-            
-            const FieldAttributes fieldAttributes = FieldAttributes.Assembly |
-                                                    FieldAttributes.Literal |
-                                                    FieldAttributes.Static |
-                                                    FieldAttributes.HasDefault;
-            var fieldDefinition = new FieldDefinition("CovarskyVersion", fieldAttributes, _assembly.MainModule.TypeSystem.String)
-            {
-                Constant = Utils.CovarskyVersion
-            };
-            td.Fields.Add(fieldDefinition);
-
-            _assembly.MainModule.Types.Add(td);
-        }
-
-        /// <summary>
-        /// Rewrites the given assembly by making variant the types that must be. 
-        /// </summary>
-        /// <param name="asm">The Cecil assembly definition to process.</param>
-        /// <param name="customInName">A custom name for the attribute that makes types contravariant.
-        /// If not specified, defaults to <see cref="ContravariantIn"/>.</param>
-        /// <param name="customOutName">A custom name for the attribute that makes types covariant.
-        /// If not specified, defaults to <see cref="CovariantOut"/>.</param>
-        /// <param name="log">A <see cref="TaskLoggingHelper"/> to communicate logging events to MSBuild.</param>
-        /// <returns>Whether the assembly was actually processed, i.e. whether any type was modified, or
-        /// the assembly has already been processed by Covarsky. The latter is discovered with a dummy type added
-        /// to the resulting assembly.</returns>
-        public static bool RewriteAssembly(AssemblyDefinition asm, string? customInName = null,
-            string? customOutName = null, TaskLoggingHelper? log = null)
-        {
-            var cr = new AssemblyRewriter(asm, customInName, customOutName, log);
-            if (cr.ShouldRewriteAssembly())
-            {
-                var wasModified = cr._types.Select(cr.ApplyVariance).Aggregate(false, (x, y) => x || y);
-                cr.MarkAsProcessedByCovarsky();
-                
-                if (!wasModified)
-                    log?.LogMessage("{0} was not modified. Skipping writing...", asm.Name.Name);
-                return wasModified;
-            }
-            log?.LogMessage("Skipping {0} as it has been already processed by Covarsky...", asm.Name.Name);
-            return false;
+            var types = asm.Modules.SelectMany(ModuleDefinitionRocks.GetAllTypes).ToList();
+            var attributeCovariant = FindSuitableAttribute(types, customOutName ?? CovariantOut);
+            var attributeContravariant = FindSuitableAttribute(types, customInName ?? ContravariantIn);
+            return
+                types
+                    .Select(t => ApplyVariance(t, attributeCovariant, attributeContravariant, log))
+                    .Aggregate(false, (x, y) => x || y);
         }
     }
 }
--- Covarsky/CovarskyTask.cs
+++ Covarsky/CovarskyTask.cs
@@ -3,75 +3,21 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-using System;
-using System.IO;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using Mono.Cecil;
+using Sigourney;
 
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace Covarsky
 {
-    public partial class CovarskyTask : Task
+    public class CovarskyTask : MSBuildWeaver
     {
-        [Required] public string? AssemblyPath { get; set; }
-        public string? OutputPath { get; set; }
-
         public string? CustomInAttributeName { get; set; }
 
         public string? CustomOutAttributeName { get; set; }
 
-        public string? IntermediateDirectory { get; set; }
-
-        public string? KeyOriginatorFile { get; set; }
-
-        public string? AssemblyOriginatorKeyFile { get; set; }
-        public bool SignAssembly { get; set; } = false;
-
-        /// <summary>
-        /// Executes the task: applies co(ntra)variance to the given assembly.
-        /// </summary>
-        /// <param name="useLog">Setting it to <c>false</c> will disable logging.
-        /// Useful for testing scenarios without the full MSBuild behind.</param>
-        public void DoExecute(bool useLog = true)
-        {
-            var outputPath = OutputPath ?? AssemblyPath;
-            // Better not write the file if nothing was written in the first place.
-            bool shouldSaveNewAssembly;
-            using var resultingAsembly = new MemoryStream();
-            using (var asm = AssemblyDefinition.ReadAssembly(AssemblyPath))
-            {
-                FindStrongNameKey(KeyOriginatorFile ?? AssemblyPath, asm);
-                shouldSaveNewAssembly = AssemblyRewriter.RewriteAssembly(asm, CustomInAttributeName,
-                    CustomOutAttributeName, useLog ? Log : null);
-                var writerParams = new WriterParameters
-                {
-                    StrongNameKeyPair = _keyPair
-                };
-                asm.Name.PublicKey = _publicKey;
-                asm.Write(resultingAsembly, writerParams);
-            }
-
-            if (Log.HasLoggedErrors || !shouldSaveNewAssembly) return;
-            using var outputFile = File.Create(outputPath);
-            resultingAsembly.Position = 0;
-            resultingAsembly.CopyTo(outputFile);
-        }
-
-        public override bool Execute()
-        {
-            try
-            {
-                DoExecute();
-            }
-            catch (Exception ex)
-            {
-                Log.LogErrorFromException(ex);
-            }
-
-            return !Log.HasLoggedErrors;
-        }
+        protected override bool DoWeave(AssemblyDefinition asm) =>
+            AssemblyRewriter.DoWeave(asm, Log2, CustomOutAttributeName, CustomInAttributeName);
     }
 }
