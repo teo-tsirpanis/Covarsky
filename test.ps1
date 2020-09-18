9260043b883431ee6db7f674996c2b99511f012f
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-09-18 22:10:32 +0300

    Run the tests thrice to see if they still fail.

--- test.ps1
+++ test.ps1
@@ -3,3 +3,4 @@ $ErrorActionPreference = "Stop"
 dotnet pack Covarsky/Covarsky.csproj -o Covarsky.Tests/test-packages /p:Version=0.0.0-test
 dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-1.binlog
 dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-2.binlog
+dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-3.binlog
