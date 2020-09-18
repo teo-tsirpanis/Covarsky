31bf6537f0f305a6a2b903fbacb95dd16c7bba36
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-09-18 22:02:15 +0300

    Run the tests against Covarsky's development release.
    It still needs to manually purge the NuGet packages cache but it's much easier that what was done in the past.
    And add AppVeyor.

---
+++ test.ps1
@@ -0,0 +1,3 @@
+dotnet pack Covarsky/Covarsky.csproj -o Covarsky.Tests/test-packages /p:Version=0.0.0-test
+dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-1.binlog
+dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-2.binlog
