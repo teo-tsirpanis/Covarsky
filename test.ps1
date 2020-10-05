e8682ac59afb5da1cb6791e3fca790c5588fbb06
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-10-05 23:57:13 +0300

    Refactor the test script and make AppVeyor fail when the tests fail.

--- test.ps1
+++ test.ps1
@@ -1,6 +1,12 @@
-$ErrorActionPreference = "Stop"
+function Test-ExitCode {if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}}
 
-dotnet pack Covarsky/Covarsky.csproj -o Covarsky.Tests/test-packages /p:Version=0.0.0-test
-dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-1.binlog
-dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-2.binlog
-dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-3.binlog
+$PackagesDir = './Covarsky.Tests/test-packages'
+
+Remove-Item $PackagesDir -Recurse
+
+dotnet pack Covarsky/Covarsky.csproj -o $PackagesDir /p:Version=0.0.0-test
+Test-ExitCode
+for ($i = 1; $i -le 3; $i++) {
+    dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-$i.binlog
+    Test-ExitCode
+}
