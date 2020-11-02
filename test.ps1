82c2f57d8ebe5acfc91d114aece68282e2a796b0
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-11-02 14:35:15 +0200

    Copy some of Sigourney's testing infrastracture.
    And add Ubuntu CI builds.

--- test.ps1
+++ test.ps1
@@ -1,10 +1,23 @@
-function Test-ExitCode {if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}}
+#! /usr/bin/env pwsh
+
+function Remove-Directory-Checked {
+    param ([string]$Directory)
+    if (Test-Path $Directory -PathType Container) {
+        Remove-Item $Directory -Recurse -Force
+    }
+}
 
 $PackagesDir = './Covarsky.Tests/packages'
+$TestLogs = './test-logs/'
+$TestProject = './Covarsky.Tests/Covarsky.Tests.fsproj'
 
+Remove-Directory-Checked $TestLogs
+dotnet clean /v:m /nodereuse:false
 dotnet pack Covarsky/Covarsky.csproj -o $PackagesDir /p:Version=0.0.0-test
-Test-ExitCode
-for ($i = 1; $i -le 3; $i++) {
-    dotnet test Covarsky.Tests/Covarsky.Tests.fsproj /bl:run-$i.binlog
-    Test-ExitCode
+
+for ($i = 1; ($i -le 3) -and ($LASTEXITCODE -eq 0); $i++) {
+    dotnet test $TestProject /nodereuse:false ("/bl:{0}run-{1}.binlog" -f $TestLogs, $i)
 }
+
+Compress-Archive $TestLogs -DestinationPath "test-logs.zip" -Force
+exit $LASTEXITCODE
