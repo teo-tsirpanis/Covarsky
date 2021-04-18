a70ad4d8b983cf59a01b7c98df0e146e452ed1e4
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2021-04-19 00:04:04 +0300

    Bring back the codes and the full stops at the end of log messages.
    However, only errors and warnings now have a code; it makes them individually suppressible (which turned out to be possible directly from MSBuild) and codes on other log message types play no significant role and bloat console output.

--- Covarsky/LogMessageExtensions.cs
+++ Covarsky/LogMessageExtensions.cs
@@ -5,43 +5,58 @@
 
 using Mono.Cecil;
 using Serilog;
+using Serilog.Sinks.MSBuild;
+
+// ReSharper disable LogMessageIsSentenceProblem
 
 namespace Covarsky
 {
     /// <summary>
     /// Extension methods for Serilog's <see cref="ILogger"/> that record Covarsky's log events.
     /// </summary>
-    internal static class LogMessageExtensions {
+    internal static class LogMessageExtensions
+    {
+        private static ILogger WithMsBuildCode(this ILogger logger, int code) => logger
+            .ForContext(MSBuildProperties.Subcategory, nameof(Covarsky))
+            .ForContext(MSBuildProperties.MessageCode, $"COVARSKY01{code:00}");
+
         public static void AttributeNamesCannotBeTheSame(this ILogger logger) =>
             logger
-                .Error("The names of Covarsky's attributes cannot be the same");
-
-        public static void NoCovariantAttributeFound(this ILogger logger) =>
-            logger
-                .Debug("No suitable attribute for marking covariant parameters was found");
-
-        public static void NoContravariantAttributeFound(this ILogger logger) =>
-            logger
-                .Debug("No suitable attribute for marking contravariant parameters was found");
+                .WithMsBuildCode(1)
+                .Error("The names of Covarsky's attributes cannot be the same.");
 
         public static void CustomAttributeNotFound(this ILogger logger, string attributeName) =>
             logger
-                .Warning("Custom attribute {AttributeName} was not found", attributeName);
+                .WithMsBuildCode(2)
+                .Warning("Custom attribute {AttributeName} was not found.", attributeName);
+
+        public static void TypeIsAlreadyVariant(this ILogger logger, string typeName, string genericParameterName) =>
+            logger
+                .WithMsBuildCode(3)
+                .Warning(
+                    "Type {TypeName}'s parameter {GenericParameterName} is already variant and Covarsky will not change it.",
+                    typeName, genericParameterName);
+
+        public static void
+            CannotDeclareBothVariances(this ILogger logger, string typeName, string genericParameterName) =>
+            logger
+                .WithMsBuildCode(4)
+                .Error(
+                    "Type {TypeName}'s parameter {GenericParameterName} cannot be declared as both covariant and contravariant.",
+                    typeName, genericParameterName);
 
         public static void MarkingType(this ILogger logger, string typeName, string genericParameterName,
             GenericParameterAttributes varianceType) =>
             logger
-                .Information("Marking type {TypeName}'s parameter {GenericParameterName} as {VarianceType}", typeName,
+                .Information("Marking type {TypeName}'s parameter {GenericParameterName} as {VarianceType}.", typeName,
                     genericParameterName, varianceType);
 
-        public static void TypeIsAlreadyVariant(this ILogger logger, string typeName, string genericParameterName) =>
+        public static void NoCovariantAttributeFound(this ILogger logger) =>
             logger
-                .Warning("Type {TypeName}'s parameter {GenericParameterName} is already variant and Covarsky will not change it",
-                    typeName, genericParameterName);
+                .Debug("No suitable attribute for marking covariant parameters was found.");
 
-        public static void CannotDeclareBothVariances(this ILogger logger, string typeName, string genericParameterName) =>
+        public static void NoContravariantAttributeFound(this ILogger logger) =>
             logger
-                .Error("Type {TypeName}'s parameter {GenericParameterName} cannot be declared as both covariant and contravariant",
-                    typeName, genericParameterName);
+                .Debug("No suitable attribute for marking contravariant parameters was found.");
     }
 }
