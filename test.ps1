0f80c8ebaa500c304c110f2f4a55b103f6575d99
Theodore Tsirpanis <teo@tsirpanis.gr>
2025-03-23 16:59:50 +0200

    Refresh infrastructure.

--- test.ps1
+++ test.ps1
@@ -1,18 +1,11 @@
 #! /usr/bin/env pwsh
 
-function Remove-Directory-Checked {
-    param ([string]$Directory)
-    if (Test-Path $Directory -PathType Container) {
-        Remove-Item $Directory -Recurse -Force
-    }
-}
-
 $PackagesDir = './Covarsky.Tests/packages'
 $TestLogs = './test-logs/'
 $TestProject = './Covarsky.Tests/Covarsky.Tests.fsproj'
 
-Remove-Directory-Checked $TestLogs
-Remove-Directory-Checked $PackagesDir
+Remove-Item $TestLogs -Recurse -Force -ErrorAction Ignore
+Get-ChildItem $PackagesDir -Filter 'Covarsky*' -ErrorAction Ignore | ForEach-Object { Remove-Item $_.FullName -Recurse -Force }
 dotnet clean /v:m /nodereuse:false
 dotnet pack Covarsky/Covarsky.csproj -o $PackagesDir /p:Version=0.0.0-test
 
